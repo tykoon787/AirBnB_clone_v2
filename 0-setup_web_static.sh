fcce257260a90fb9aef8e3a0949405b064a7fc87
tykoon787 <tykoon787@protonmail.com>
2023-04-21 19:02:21 +0300

    Bash Script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,26 @@
 #!/usr/bin/env bash
 # This script sets up a static web server
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-## Install nginx
-sudo apt-get install -y nginx
-
-## Create directories and sub-dirs
-### releases
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
-
-### shared
-sudo mkdir -p /data/web_static/shared
-
-### releases/test
-sudo mkdir -p /data/web_static/releases/test
-
-### Index.html
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo sed -i '$ a\<html><\n\t<title><p>Panda Practice</p></title>\n\t\t<body><h1>This is the body</h1>\n\t\t</body>\n</html>' /data/web_static/releases/test/index.html
 
 ## Create a symbolic link
 sudo mkdir -p /data/web_static/current
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 ## Changing ownership
 sudo chown -R ubuntu:ubuntu /data/
 
 ## Serving content of /data/web_static/current/
-sudo sed -i '/server_name _;/a\\tlocation /hbnb_static {\n\t\t alias /data/web/static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a\\tlocation /hbnb_static {\n\t\t alias /data/web/static/current/;\n\t}' /etc/nginx/sites-available/default
 
 ## Restart Service
 sudo nginx -s reload
