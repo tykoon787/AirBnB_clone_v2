51cabe405dc0daf09bef36904e8ecb7dd730a79f
tykoon787 <tykoon787@protonmail.com>
2023-04-22 02:59:24 +0300

    Bash Script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,17 +12,16 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo sed -i '$ a\<html><\n\t<title><p>Panda Practice</p></title>\n\t\t<body><h1>This is the body</h1>\n\t\t</body>\n</html>' /data/web_static/releases/test/index.html
 
+## Changing ownership
+sudo chown -R ubuntu:ubuntu /data/
+
 ## Create a symbolic link
 sudo mkdir -p /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-## Changing ownership
-sudo chown -R ubuntu:ubuntu /data/
 
 ## Serving content of /data/web_static/current/
 sudo sed -i '/listen 80 default_server/a\\tlocation /hbnb_static {\n\t\talias /data/web/static/current/;\n\t}' /etc/nginx/sites-available/default
 
 ## Restart Service
 sudo nginx -s reload
-
-echo "This script finished"
+sudo service nginx reload
