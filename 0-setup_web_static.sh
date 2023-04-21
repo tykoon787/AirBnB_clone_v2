e46f8fd578ba23333206275c600300a1acb34e3a
tykoon787 <tykoon787@protonmail.com>
2023-04-21 11:19:01 +0300

    Created Exectutable Script '0-setup_web_static.sh'

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,27 +6,30 @@ sudo apt-get install nginx
 
 ## Create directories and sub-dirs
 ### releases
-mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/
 
 ### shared
-mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/shared
 
 ### releases/test
-mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/releases/test
 
 ### Index.html
-touch /data/web_static/releases/test/index.html
-sed -i '$ a\<html><\n\t<title><p>Panda Practice</p></title>\n\t\t<body><h1>This is the body</h1>\n\t\t</body>\n</html>' index.html
+sudo touch /data/web_static/releases/test/index.html
+sudo sed -i '$ a\<html><\n\t<title><p>Panda Practice</p></title>\n\t\t<body><h1>This is the body</h1>\n\t\t</body>\n</html>' /data/web_static/releases/test/index.html
 
 ## Create a symbolic link
-mkdir /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 ## Changing ownership
 sudo chown -R ubuntu:ubuntu /data/
 
 ## Serving content of /data/web_static/current/
-sed -i '/server_name _;/a\\tlocation /hbnb_static {\n\t\t alias /data/web/static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a\\tlocation /hbnb_static {\n\t\t alias /data/web/static/current/;\n\t}' /etc/nginx/sites-available/default
 
 ## Restart Service
 sudo service nginx restart
+if [ $? -ne 0 ]; then
+    sudo nginx -s reload
+fi
