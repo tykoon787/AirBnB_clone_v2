74b5a2def72cc43cfaf9a9e330cbc943150f43c0
tykoon787 <tykoon787@protonmail.com>
2023-05-05 01:33:34 +0300

    Updated Code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """Function to Return '/' route"""
-    return('"Hello HBNB!"')
+    return("Hello HBNB!")
 
 
 if __name__ == "__main__":
