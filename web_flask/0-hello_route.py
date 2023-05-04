50c4272859f7e65258d33955815f41712c376eed
tykoon787 <tykoon787@protonmail.com>
2023-05-05 01:24:33 +0300

    Flask Python Script 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module starts a flask app on port 0.0.0.0 
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Function to Return '/' route"""
+    return('"Hello HBNB!"')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5050, debug=True)
